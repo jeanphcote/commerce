6f80723fd7a3db84d92bf09ea970dc622b059257
jeanphcote <jeanphcote001@gmail.com>
2022-04-08 16:45:39 +0200

    added Listing and Bid models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=140)
+    picture_url = models.URLField()
+    closed = models.BooleanField(default=False)
+
+class Bid(models.Model):
+    bid_price = models.FloatField()
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE)
